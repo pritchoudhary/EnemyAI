dda9d8ffb4a06447176ded859ae2e0deb3f715f0
Pritish Choudhary <pritish.choudhary@yahoo.in>
2016-07-09 14:10:38 +1000

    Added a basic AI. Isuues with the enemy animation. Not moving as intended so encountering problems in checking the validity of the code.

---
+++ Assets/Scripts/basicAI.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class basicAI : MonoBehaviour
+    {
+
+        public NavMeshAgent agent;
+        public ThirdPersonCharacter character;
+
+        public enum State
+        {   
+            PATROL,
+            CHASE
+        }
+
+        public State state;
+        private bool alive;
+
+        //Variables for patrolling
+        public GameObject[] wayPoints;
+        private int wayPointIndex = 0;
+        public float patrolSpeed = 0.5f;
+
+        //Variables for chasing
+        public float chaseSpeed = 1f;
+        public GameObject target;
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+
+            //Set initial state
+            state = basicAI.State.PATROL;
+
+            alive = true;
+
+            //Start Finite State Machine
+            StartCoroutine("FiniteStateMachine");
+        }
+
+        IEnumerator FiniteStateMachine()
+        {
+            //run while alive
+            while(alive)
+            {
+                switch(state)
+                {
+                    case State.PATROL:
+                        Patrol();
+                        break;
+
+                    case State.CHASE:
+                        Chase();
+                        break;
+                 }
+                yield return null;
+            }
+        }
+
+
+        void Patrol()
+        {
+            agent.speed = patrolSpeed;
+
+            //if too far move closer
+            if(Vector3.Distance(this.transform.position, wayPoints[wayPointIndex].transform.position) >= 2)
+            {
+                agent.SetDestination(wayPoints[wayPointIndex].transform.position);
+                character.Move(agent.desiredVelocity, false, false);
+
+            }
+
+            //if too close move to a different way point
+            else if(Vector3.Distance(this.transform.position, wayPoints[wayPointIndex].transform.position) <= 2)
+            {
+                //increase the counter
+                wayPointIndex += 1;
+
+                // Avoid over running of waypoints
+                if(wayPointIndex > wayPoints.Length)
+                {
+                    wayPointIndex = 0;
+                }
+            }
+
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+        //Run after the target
+        void Chase()
+        {
+            agent.speed = chaseSpeed;
+
+            agent.SetDestination(target.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if(collider.tag == "Player")
+            {
+                state = basicAI.State.CHASE;
+                target = collider.gameObject;
+            }
+        }
+
+    }
+}
+
